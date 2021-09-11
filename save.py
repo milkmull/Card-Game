9854be1909f9c9236b02b1b9c5abdb2f260af087
milkmull <mikepmull2.com>
2021-09-11 17:26:38 -0400

    save folder now created on startup

--- save.py
+++ save.py
@@ -155,7 +155,7 @@ def verify_data():
     elif any(settings.get(key) not in base_settings[key] for key in base_settings):
         settings = {'rounds': 3, 'ss': 20, 'cards': 5, 'items': 3, 'spells': 1, 'cpus': 1, 'diff': 4}
         set_data('settings', settings)
-
-SAVE_DATA = load_save()
+        
 create_folders()
+SAVE_DATA = load_save()
 verify_data()
